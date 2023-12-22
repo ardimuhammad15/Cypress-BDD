aa889fb16263bb850739a506ddbbf37af0525b03
ardimuhammad15 <ardimuhammad085@gmail.com>
2023-12-22 18:33:05 +0700

    Step Definition

---
+++ loginTest.js
@@ -0,0 +1,19 @@
+// import LoginPage from './loginTest.page'
+const { Given, When, Then } = require('@badeball/cypress-cucumber-preprocessor')
+
+Given('I open login page', () => {
+	cy.visit('http://zero.webappsecurity.com/login.html')
+})
+
+When('I submit login', () => {
+	// LoginPage.fillUsername('username')
+	// LoginPage.fillPassword('password')
+	// LoginPage.signIn()
+	cy.get('#user_login').type('username')
+	cy.get('#user_password').type('password')
+	cy.contains('Sign in').click()
+})
+
+Then('I should see homepage', () => {
+	cy.get('#account_summary_tab > a').should('be.visible')
+})
